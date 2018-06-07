0412ec67bc683f5cdb9e005f991c5310c7898207
Hugo DURAND <Hugo.DURAND@edugroupe.com>
2018-06-07 11:21:56 +0200

    update sql

--- sql/jee_project.sql
+++ sql/jee_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 06 juin 2018 à 13:24
+-- Généré le :  jeu. 07 juin 2018 à 09:20
 -- Version du serveur :  5.7.19
 -- Version de PHP :  5.6.31
 
@@ -32,20 +32,22 @@ DROP TABLE IF EXISTS `taches`;
 CREATE TABLE IF NOT EXISTS `taches` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `urgent` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `taches`
 --
 
-INSERT INTO `taches` (`id`, `libelle`, `id_user`, `date`, `urgent`) VALUES
-(2, 'Tache1', 1, '2018-06-07', 1),
-(3, 'Tache2', 1, '2018-06-22', 0),
-(4, 'Tache3', 1, '2018-06-30', 1);
+INSERT INTO `taches` (`id`, `libelle`, `user`, `date`, `urgent`) VALUES
+(2, 'Tache1', 'DURAND Hugo', '2018-06-07', 1),
+(3, 'Tache2', 'DURAND Hugo', '2018-06-22', 0),
+(4, 'Tache3', 'DURAND Hugo', '2018-06-30', 1),
+(5, 'Taches4', 'DURAND Hugo', '2018-05-02', 1),
+(6, 'Taches5', 'DURAND Hugo', '2018-12-02', 0);
 
 -- --------------------------------------------------------
 
